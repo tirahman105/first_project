9afaed2b17619233bd4b554e48a9bc46272575f9
tirahman105 <tirahman105@gmail.com>
2023-11-20 19:41:51 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+// const port = 3000
+
+//parser
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
+
+// console.log(process.cwd());
